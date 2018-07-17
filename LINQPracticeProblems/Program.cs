c660f56cbe5f231fb717a902b78a084aba138eeb
Sam <SSUrbancic@outlook.com>
2018-07-17 15:44:04 -0500

    First I created a classeGrades list and entered four different strings. Then I created a var called overallAverage and started by using Select to implement a split on every element breaking down the string into seperate parts. Now that they had all been split I used Select again and implemented ConvertAll(grades, double.Parse) to change the strings in each element into an array. Using Select again I created an equation to add the arrays together, subtract the lowest grade and then find the average of each array. I did this by using (grades.Sum() - grades.Min()) / (grades.Count() - 1). Had to implement the - 1 after the count since the count of each array was technically still five and we are only trying to find the average of 4 elements since the lowest grade was subtracted. Now the list that was filled with strings is a list of integers which means .Average() as the last extension to find the average of all the averages.

--- LINQPracticeProblems/Program.cs
+++ LINQPracticeProblems/Program.cs
@@ -10,22 +10,36 @@ namespace LINQPracticeProblems
     {
         static void Main(string[] args)
         {
-            //Return list that contains substrings
-            List<string> wordList = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
-            var newWordList = wordList.Where(word => word.Contains("th"));
-            foreach (var word in newWordList)
-            {
-                Console.WriteLine(word);
-            }
-            Console.ReadLine();
+            ////Return list that contains substrings
+            //List<string> wordList = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            //var newWordList = wordList.Where(word => word.Contains("th"));
+            //foreach (var word in newWordList)
+            //{
+            //    Console.WriteLine(word);
+            //}
+            //Console.ReadLine();
 
-            //Return a copy of a list without any duplicates
-            List<string> nameList = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike", "Mike", "Nick" };
-            var newNameListNoDuplicates = nameList.GroupBy(name => name).Select(name => name.First());
-            foreach (var name in newNameListNoDuplicates)
+            ////Return a copy of a list without any duplicates
+            //List<string> nameList = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike", "Mike", "Nick" };
+            //var newNameListNoDuplicates = nameList.GroupBy(name => name).Select(name => name.First());
+            //foreach (var name in newNameListNoDuplicates)
+            //{
+            //    Console.WriteLine(name);
+            //}
+            //Console.ReadLine();
+
+
+            List<string> classGrades = new List<string>()
             {
-                Console.WriteLine(name);
-            }
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+            };
+            var listOfAverages = classGrades.Select(grades => grades.Split(',')).Select(grades => Array.ConvertAll(grades, double.Parse)).Select(grades => (grades.Sum() - grades.Min()) / (grades.Count() - 1)).Average();
+
+            Console.WriteLine(listOfAverages);
+
             Console.ReadLine();
         }
     }
