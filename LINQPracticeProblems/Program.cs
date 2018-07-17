e135b8755d5f7917d57b2a8bf62fc75c77020714
Sam <SSUrbancic@outlook.com>
2018-07-17 13:38:44 -0500

    Made a list called nameList that contained 7 names with 3 Mikes and 2 Nicks. Then created a var called newNameListNoDuplicates. I set that equal to nameList.GroupBY(name => name).Select(name => name.First()). GroupBy will take all the names from the original list and group them by names, then Select combined with name.First() will take the first instance in each group and put them in the new list, avoiding any duplicate instances. Verified correct functionality with a foreach loop.

--- LINQPracticeProblems/Program.cs
+++ LINQPracticeProblems/Program.cs
@@ -10,14 +10,23 @@ namespace LINQPracticeProblems
     {
         static void Main(string[] args)
         {
+            //Return list that contains substrings
             List<string> wordList = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
             var newWordList = wordList.Where(word => word.Contains("th"));
-            foreach(var word in newWordList)
+            foreach (var word in newWordList)
             {
                 Console.WriteLine(word);
             }
             Console.ReadLine();
-        }
 
+            //Return a copy of a list without any duplicates
+            List<string> nameList = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike", "Mike", "Nick" };
+            var newNameListNoDuplicates = nameList.GroupBy(name => name).Select(name => name.First());
+            foreach (var name in newNameListNoDuplicates)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+        }
     }
 }
